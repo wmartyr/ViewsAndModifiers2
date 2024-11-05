8d3391690c311f5f438ec9f375748e6e04dd0664
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-11-06 06:55:46 +1030

    Initial Commit

---
+++ ViewsAndModifiers2/ViewsAndModifiers2App.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiers2App.swift
+//  ViewsAndModifiers2
+//
+//  Created by Woodrow Martyr on 6/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiers2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ViewsAndModifiers2UITests/ViewsAndModifiers2UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ViewsAndModifiers2UITestsLaunchTests.swift
+//  ViewsAndModifiers2UITests
+//
+//  Created by Woodrow Martyr on 6/11/2024.
+//
+
+import XCTest
+
+final class ViewsAndModifiers2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
